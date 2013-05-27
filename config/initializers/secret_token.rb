1257fe7ff67f88157c78446ee90b867c886d78e5
Tonatiuh Nunez <tonatiuhn@hotmail.es>
2013-05-26 22:43:48 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pinpong::Application.config.secret_key_base = '9fbfef6f8c687f69c028a8f349dd5dd0c8c8563c16fa0a84c16fc5b1ca4df81736339cde22a8634ef20b0a019db98b0bdeabac78195ac0f7277138a0224f129a'
