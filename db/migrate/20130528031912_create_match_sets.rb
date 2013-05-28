484082c39bea00fdb6210ebbf5da2575cb7cc28d
Alberto Alcaraz <alberto.alcaraz@tangosource.com>
2013-05-28 02:36:09 -0500

    added set model and basic specs

---
+++ db/migrate/20130528031912_create_match_sets.rb
@@ -0,0 +1,10 @@
+class CreateMatchSets < ActiveRecord::Migration
+  def change
+    create_table :match_sets do |t|
+      t.integer :match_id
+      t.integer  :team_id
+
+      t.timestamps
+    end
+  end
+end
