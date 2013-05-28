422a038759aa3dc61c8ad8a57a84c864a452c8f9
DanielLL <daniel.lopez@tangosource.com>
2013-05-28 11:24:18 -0500

    refactored factories and correction of tests

--- spec/factories/team.rb
+++ spec/factories/team.rb
@@ -4,5 +4,10 @@ FactoryGirl.define do
   factory :valid_team, :class => Team do |p|
     name "Test"
   end
+
+  factory :team_with_players, :class => Team do |t|
+    name "Test"
+    players {[FactoryGirl.create(:valid_player)]}
+  end
 end
 
--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -12,25 +12,20 @@ describe Team do
 
   end
 
-  describe "a valid team" do
+  describe "creation of a valid team" do
     let(:team) { FactoryGirl.build(:valid_team) }
 
     it "should return the name of the team" do
-      team.save
-      team.name.should  == "Test"
-      Team.count.should == 1
+      team.save.should be(true)
     end
 
   end
 
   describe "a valid team with multiple players on it" do
-    let(:team) { FactoryGirl.create(:valid_team) }
+    let(:team) { FactoryGirl.create(:team_with_players) }
 
     it "should return the players related with the team" do
-      team.players.create(name: "Test 1", type_account: "test_account", user_account: "user_account", picture_url: { small: "profile_picture_small.png", normal: "profile_picture_normal.png", big: "profile_picture_big.png"})
-      team.players.create(name: "Test 2", type_account: "test_account", user_account: "user_account", picture_url: { small: "profile_picture_small.png", normal: "profile_picture_normal.png", big: "profile_picture_big.png"})
       team.players.first.name.should == Player.first.name
-      team.players.last.name.should  == Player.last.name
     end
 
   end
