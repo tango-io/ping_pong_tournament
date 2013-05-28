dcca6bf776bc2e94e0572672d9aa6a9fe9135e00
Alberto Alcaraz <alberto.alcaraz@tangosource.com>
2013-05-28 05:14:44 -0500

    set first api controller

---
+++ app/controllers/api/match_controller.rb
@@ -0,0 +1,9 @@
+class Api::MatchController < ApplicationController
+  def show
+    @matches = MatchRound.where(number: params[:id]).shift.matches
+
+    respond_to do | format |
+      format.json { render json: { matches: @matches } }
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Pinpong::Application.routes.draw do
   root to: "application#index"
+
+  namespace :api do
+    resources :match, only: [ :show ]
+  end
 end
