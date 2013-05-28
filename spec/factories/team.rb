abff9703f0f46cad54dc46d6e9430e6c817cb5a1
DanielLL <daniel.lopez@tangosource.com>
2013-05-28 11:26:21 -0500

    hot fix of the team_with_players factory

--- spec/factories/team.rb
+++ spec/factories/team.rb
@@ -1,13 +1,12 @@
 FactoryGirl.define do 
   factory :invalid_team, :class => Team do |p|
   end
+
   factory :valid_team, :class => Team do |p|
     name "Test"
-  end
-
-  factory :team_with_players, :class => Team do |t|
-    name "Test"
-    players {[FactoryGirl.create(:valid_player)]}
+    factory :team_with_players, :class => Team do |t|
+      players {[FactoryGirl.create(:valid_player)]}
+    end
   end
 end
 
