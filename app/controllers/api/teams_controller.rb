24d1997acf676252f15be9e8b96d7378ad7da0b6
DanielLL <daniel.lopez@tangosource.com>
2013-05-29 14:25:43 -0500

    Refactored teams controller and it spec

--- app/controllers/api/teams_controller.rb
+++ app/controllers/api/teams_controller.rb
@@ -1,6 +1,5 @@
 class Api::TeamsController < ApplicationController
   layout false
-  respond_to :json
 
   expose(:teams)
   expose(:team)
@@ -13,13 +12,7 @@ class Api::TeamsController < ApplicationController
   end
 
   def create
-    team = Team.new
-    team.assign_attributes(name: params["team"]["name"], picture: JSON.parse(params["team"]["picture"].to_json))
-
-    params["team"]["players"].each do |player_attributes|
-      team.players.new(player_attributes)
-    end
-
+    team = Team.new(team_params)
     if team.save
       render json: { message: "The Team was successfully created"}
     else
@@ -31,6 +24,11 @@ class Api::TeamsController < ApplicationController
     if team.delete
       render json: { message: "The Team was successfully deleted"}
     end
+  end
+
+  private
+  def team_params
+    params.require(:team).permit(:name, :picture, players_attributes: [:name, :type_account, :user_account, :email, :picture_url])
   end
 
 end
--- spec/controllers/api/teams_controller_spec.rb
+++ spec/controllers/api/teams_controller_spec.rb
@@ -3,37 +3,43 @@ require 'spec_helper'
 describe Api::TeamsController do
 
   describe "team json requests" do
-    let(:team) { FactoryGirl.create(:team) }
+    let(:team) { FactoryGirl.build(:team) }
 
     it "returns a team" do
+      team.save
       get :show, { :id => team.id, :format => :json, team: team}
 
       response.body.should == { team: team }.to_json
     end
 
-    it "returns the correct message when a team was created" do
-      team_attributes = team.attributes
-      team_attributes.merge!(players: [team.players.first.attributes, team.players.last.attributes])
-      post :create, { team: team_attributes }, format: :json
+    context 'With the correct message' do
+      it "returns the correct message when a team was created" do
+        team_attributes = { name: team.name, picture: team.picture, players_attributes: [team.players.first.attributes, team.players.last.attributes] }
+        post :create, { team: team_attributes }, format: :json
 
-      JSON.parse(response.body)["message"].should == "The Team was successfully created"
+        JSON.parse(response.body)["message"].should == "The Team was successfully created"
+        Team.last.players.count.should == 2
+      end
+
+      it "returns the correct message when a team was not created" do
+        team_attributes    = team.attributes
+        team_attributes.merge!(players_attributes: [team.players.first.attributes.reject!{|k| k == "name" }, team.players.last.attributes])
+        post :create, { team: team_attributes }, format: :json
+
+        JSON.parse(response.body)["message"].should == "The Team was not created"
+      end
+
+      it "returns the correct message when a team is deleted" do
+        team.save
+
+        Team.count.should == 1
+        delete :destroy, { :id => team.id, :format => :json, team: team}
+        Team.count.should == 0
+        JSON.parse(response.body)["message"].should == "The Team was successfully deleted"
+      end
+
     end
-
-    it "returns the correct message when a team was not created" do
-      team_attributes    = team.attributes
-      team_attributes.merge!(players: [team.players.first.attributes.reject!{|k| k == "name" }, team.players.last.attributes])
-      post :create, { team: team_attributes }, format: :json
-
-      JSON.parse(response.body)["message"].should == "The Team was not created"
-    end
-
-    it "returns the team with the id specified" do
-      team.save
-
-      delete :destroy, id: 1
-      JSON.parse(response.body)["message"].should == "The Team was successfully deleted"
-    end
-
+    
   end
 
 end
