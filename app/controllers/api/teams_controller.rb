ddcba80a3cdbb793601dbf30359f01fdb5c7ad0d
DanielLL <daniel.lopez@tangosource.com>
2013-05-29 02:21:10 -0500

    Refactored teams controller, added destroy action and its test

--- app/controllers/api/teams_controller.rb
+++ app/controllers/api/teams_controller.rb
@@ -4,25 +4,31 @@ class Api::TeamsController < ApplicationController
   expose(:team)
 
   def index
-    respond_with teams
+    respond_with teams: teams
   end
 
   def show
-    respond_with team
+    respond_with team: team
   end
 
   def create
-    @team = Team.new
-    @team.assign_attributes(name: params["team"]["name"], picture: JSON.parse(params["team"]["picture"].to_json))
+    team = Team.new
+    team.assign_attributes(name: params["team"]["name"], picture: JSON.parse(params["team"]["picture"].to_json))
 
     params["team"]["players"].each do |player_attributes|
-      @team.players.new(player_attributes)
+      team.players.new(player_attributes)
     end
 
-    if @team.save
+    if team.save
       render json: { message: "The Team was successfully created"}
     else
       render json: { message: "The Team was not created"}
+    end
+  end
+
+  def destroy
+    if team.delete
+      render json: { message: "The Team was successfully deleted"}
     end
   end
 
