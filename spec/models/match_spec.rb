c9ee642ec2208a4618b6b2fbaf961c2f11226aad
Alberto Alcaraz <alberto.alcaraz@tangosource.com>
2013-05-29 06:23:48 -0500

    added function to return winner of the match

--- spec/models/match_spec.rb
+++ spec/models/match_spec.rb
@@ -2,27 +2,42 @@ require 'spec_helper'
 
 describe Match do
 
-  before do
-    @match = Match.first
-    for n in 1..2 do
-      team = FactoryGirl.build(:team, id: n)
-      team.matches << @match
-      team.save
-    end
-  end
+  let(:round) { FactoryGirl.create(:round) }
+  let(:match) { FactoryGirl.create(:match, match_round: round ) }
 
   context "should have" do
     it "a position" do
-      @match.match_number.should_not be_nil
+      match.match_number.should_not be_nil
     end
 
     it "3 sets once it start" do
-      @match.start
-      @match.match_sets.count.should == 3
+      match.start
+      match.match_sets.count.should == 3
     end
 
-    it "one round" do
-      @match.match_round.should == MatchRound.round_of_16
+    it "a round" do
+      match.match_round.should == round
+    end
+
+    it "a winner team" do
+      match.start
+
+      team = match.teams.first
+      sets = match.match_sets
+
+      sets[0].scores.first.update_attributes(total: 11)
+      sets[0].scores.last.update_attributes(total: 3)
+
+      sets[1].scores.first.update_attributes(total: 9)
+      sets[1].scores.last.update_attributes(total: 11)
+
+      sets[2].scores.first.update_attributes(total: 11)
+      sets[2].scores.last.update_attributes(total: 5)
+
+      sets.each { | set | set.set_winner }
+
+      match.reload
+      match.match_winner.should == team
     end
   end
 
