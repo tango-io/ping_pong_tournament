dddaeeb4fb9efbacb151bce75adf80dab2a32183
DanielLL <daniel.lopez@tangosource.com>
2013-05-28 20:38:17 -0500

    Added teams controller and its tests and also its routes

---
+++ app/controllers/api/teams_controller.rb
@@ -0,0 +1,29 @@
+class Api::TeamsController < ApplicationController
+  respond_to :json, :xml    
+  expose(:teams)
+  expose(:team)
+
+  def index
+    respond_with teams
+  end
+
+  def show
+    respond_with team
+  end
+
+  def create
+    @team = Team.new
+    @team.assign_attributes(name: params["team"]["name"], picture: JSON.parse(params["team"]["picture"].to_json))
+
+    params["team"]["players"].each do |player_attributes|
+      @team.players.new(player_attributes)
+    end
+
+    if @team.save
+      render json: { message: "The Team was successfully created"}
+    else
+      render json: { message: "The Team was not created"}
+    end
+  end
+
+end
