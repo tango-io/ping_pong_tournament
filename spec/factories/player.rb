d3b053546a4a6304fc125584c1d633a81d2548bf
DanielLL <daniel.lopez@tangosource.com>
2013-05-29 02:18:54 -0500

    Refactored player and team factories

--- spec/factories/player.rb
+++ spec/factories/player.rb
@@ -5,5 +5,10 @@ FactoryGirl.define do
     sequence(:type_account) {|n| "Twitter #{n}" }
     sequence(:user_account) {|n| "Test #{n}" }
     sequence(:email)        {|n| "Test#{n}@test.com" }
+    picture_url {{
+      small: "profile_picture_small.png",
+      normal: "profile_picture_normal.png",
+      big: "profile_picture_big.png"
+    }}
   end
 end
