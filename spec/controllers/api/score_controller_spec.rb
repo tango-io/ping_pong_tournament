6202dfa6c77e42b408307832e500e338bce193b3
Alberto Alcaraz <alberto.alcaraz@tangosource.com>
2013-05-29 05:29:11 -0500

    added specs for api, moved some spec that should be on other controller

---
+++ spec/controllers/api/score_controller_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe Api::ScoreController do
+  let(:match) { FactoryGirl.create(:match) }
+
+  it "should set score for each match set" do
+    match.start
+    set_score = match.match_sets.first.scores.first
+
+    put :update, id: set_score.id, total: 11, format: :json
+
+    set_score.reload
+    set_score.total.should == 11
+  end
+
+end
