ff6ae2f7a6f2bdacaa124c4f59b6642b806c586e
DanielLL <daniel.lopez@tangosource.com>
2013-05-28 20:37:07 -0500

    Refactored factories for players and teams

--- spec/factories/team.rb
+++ spec/factories/team.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do 
   factory :team, :class => Team do |p|
     name "Test"
-    players {[ FactoryGirl.build(:player, picture_url: { small: "profile_picture_small.png", normal: "profile_picture_normal.png", big: "profile_picture_big.png"}) ]}
+    players {[ FactoryGirl.build(:player, picture_url: { small: "profile_picture_small.png", normal: "profile_picture_normal.png", big: "profile_picture_big.png"}), FactoryGirl.build(:player, picture_url: { small: "profile_picture_small.png", normal: "profile_picture_normal.png", big: "profile_picture_big.png"}) ]}
   end
 end
 
