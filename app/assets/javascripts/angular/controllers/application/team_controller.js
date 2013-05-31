05a9e96794f06707767ebffcb07b9bf48c9d6c6f
Alberto Alcaraz <alberto.alcaraz@tangosource.com>
2013-05-31 04:55:04 -0500

    deleted function and renamed another

--- app/assets/javascripts/angular/controllers/application/team_controller.js
+++ app/assets/javascripts/angular/controllers/application/team_controller.js
@@ -1,15 +1,10 @@
 App.controller('TeamController', ['$scope', '$http', '$location', '$routeParams', function($scope, $http, $location, $routeParams){
   $scope.team = [];
 
-  $scope.showRound = (function(){
+  $scope.showTeam = (function(){
     var id = $routeParams.id;
     $http.get('/api/teams/' + id).success(function(data){
       $scope.team = data.team;
-      console.log($scope.team);
     });
   }).call(this);
-
-  $scope.showTeam = function(id){
-    $location.path('team/'+id);
-  };
 }]);
