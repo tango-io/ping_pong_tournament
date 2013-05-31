e99944f0859b20f8a485b3d69e201d08b19056a4
DanielLL <daniel.lopez@tangosource.com>
2013-05-31 10:59:01 -0500

    Refactored factories and migration, name field removed

--- db/migrate/20130527183722_create_players.rb
+++ db/migrate/20130527183722_create_players.rb
@@ -1,7 +1,6 @@
 class CreatePlayers < ActiveRecord::Migration
   def change
     create_table :players do |t|
-      t.string  :name
       t.string  :type_account
       t.string  :user_account
       t.string  :email
