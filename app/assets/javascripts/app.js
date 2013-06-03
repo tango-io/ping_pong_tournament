9979ad6b7a302cc5c73af89cf32f603b8bcfe279
DanielLL <daniel.lopez@tangosource.com>
2013-06-03 12:21:55 -0500

    Moved TweeterFeed to the layout

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -3,19 +3,40 @@ var App = angular.module('PingPong', ['ngResource'])
 App.config(['$routeProvider', function($routeProvider){
   $routeProvider.
     when('/round/final_match', {
-      controller: "TemplatesController",
-      templateUrl: 'api/templates/final_match'
-    })
-    .when('/round/:id', {
-      controller: "TemplatesController",
-      templateUrl: "api/templates/round"
-    })
-    .when('/team/:id', {
-      controller: "TemplatesController",
-      templateUrl: 'api/templates/team'
-    })
-    .when('/match/:id', {
-      controller: "TemplatesController",
-      templateUrl: 'api/templates/match'
-    });
+    controller: "TemplatesController",
+    templateUrl: 'api/templates/final_match'
+  })
+  .when('/round/:id', {
+    controller: "TemplatesController",
+    templateUrl: "api/templates/round"
+  })
+  .when('/team/:id', {
+    controller: "TemplatesController",
+    templateUrl: 'api/templates/team'
+  })
+  .when('/match/:id', {
+    controller: "TemplatesController",
+    templateUrl: 'api/templates/match'
+  });
+
+  $('#jTweetsAnywhereEndlessScrollingSample').jTweetsAnywhere({
+    username: 'magmaconf',
+    count: 10,
+    showTweetFeed: {
+      showProfileImages: true,
+      showUserScreenNames: true,
+      paging: {
+        mode: 'endless-scroll'
+      }
+    },
+    onDataRequestHandler: function(stats) {
+      if (stats.dataRequestCount < 11) {
+        return true;
+      }
+      else {
+        alert("To avoid struggling with Twitter's rate limit, we stop loading data after 10 API calls.");
+      }
+    }
+  });
+
 }]);
